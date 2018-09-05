1974ebb8c36e4e29db480bfa82963b361d531bac
Georgios Arampatzis <garampat@ethz.ch>
2018-09-05 16:05:29 +0200

    change a little the LV model

--- matlab/p03_lotka_volterra.m
+++ matlab/p03_lotka_volterra.m
@@ -1,13 +1,15 @@
 clear
 
 a = 0.5;
-b = 10;
+b = 0.01;
 c = 0.1;
-d = 10;
+d = 0.01;
+
 
 tspan = [0 100];
-y0 = [10 ; 20];
-F = @(t,y) [ a*y(1)*(1-y(2)/b) ; -c*y(2)*(1-y(1)/d) ];
+y0 = [ 10 ; 25 ];
+F = @(t,y) [ y(1)*(a-b*y(2)) ; y(2)*(-c+d*y(1)) ];
+
 
 
 sol = ode45( F, tspan, y0 );
@@ -16,10 +18,10 @@ t = linspace( tspan(1), tspan(2), 5000)';
 y = deval(sol,t);
 
 
-%% PLOT
+% PLOT
 
 figure(1);
-axis([0 100 0 40])
+axis([0 100 0 90])
 xlabel('time')
 set(gca,'FontSize',20)
 grid on
@@ -27,6 +29,7 @@ grid on
 % just plot
 % plot( t, y, '-', 'LineWidth', 3 )
 
+%%
 % animated plot
 h1 = animatedline('LineWidth',3,'Color','red');
 h2 = animatedline('LineWidth',3,'Color','black');
